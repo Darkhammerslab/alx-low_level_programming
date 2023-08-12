4ca92ab7a92cce8dcf7bee002f394717898a1195
Darkhammerslab <kabitamichael392@gmail.com>
2023-08-12 05:56:21 -0700

    for putchar

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print combinations of two digits'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens == 48; tens <= 57; tens++)
+	{
+		putchar(tens);
+		tens++;
+	}
+	for (ones == 48; ones <= 57; ones++)
+	{
+		putchar(ones);
+		ones++;
+	}
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
