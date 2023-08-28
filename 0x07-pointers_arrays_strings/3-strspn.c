0a2f82148e75da30f1c5ae3aa8ecb1bea570cd36
Darkhammerslab <kabitamichael392@gmail.com>
2023-08-28 16:07:24 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,31 @@
 #include "main.h"
 /**
  * _strspn - gets the length of a prefix substring
+ * @a: string
  * @s: string
- * @accept: input
+ * @count: input
  * Return: number of bytes 
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+	unsigned int count = 0;
 
-	value = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		check = 0;
-
-		for (n = 0; accept[n] != '\0'; n++)
+		char *a = accept;
+	
+		while (*a)
 		{
-			if (accept[n] == s[i])
+			if (*s == *a)
 			{
-				value++;
-				check = 1;
+				count++;
+				break;
 			}
+			a++;
 		}
+		if (*a == '\0')
+			break;
+		s++;
 	}
+	return (count);
+}
