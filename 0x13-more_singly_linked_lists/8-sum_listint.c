771da40c0aa3d2dddfe79336eee4c180e38d2b28
Darkhammerslab <kabitamichael392@gmail.com>
2023-09-25 12:46:27 -0700

    sum of data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint - return sum of all data of a list
+ * @head: first node
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
