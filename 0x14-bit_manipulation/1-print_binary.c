d1ae180ad7cb14370109d72ddee1d7aefaa2323f
Darkhammerslab <kabitamichael392@gmail.com>
2023-09-28 13:00:08 -0700

    binary rep

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - print binary representation of number
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int c;
+
+	for (i = 63; i >= 0; i--)
+	{
+		c = n >> i;
+
+		if (c & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
